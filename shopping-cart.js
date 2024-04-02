f2757fbf1d5db7646a1dad1af07e02f25b654c19
Glenn <glennedwardvilalflor@gmail.com>
2024-04-02 18:38:32 +0800

    Added Displaying of Sub-total and Discount Computation

--- shopping-cart.js
+++ shopping-cart.js
@@ -1,27 +1,66 @@
-alert("hello");
+alert("hello")
+// Get elements from the DOM
+const addBtn = document.getElementById('addApple');
+const removeBtn = document.getElementById('removeApple');
+const display = document.getElementById('display');
+const price = document.getElementById('price');
+const total = document.getElementById('total');
 
-let counterDisplayElem = document.getElementById('display');
-let counterMinusElem = document.getElementById('remove');
-let counterPlusElem = document.getElementById('add');
-let priceDisplayElem = document.getElementById('price');
-let appleValue = 100.00;
-// let orangeValue = 125.00;
-// let grapeValue = 150.00;
+// Get elements for the Grapes Juice item from the DOM
+const addGrapesBtn = document.getElementById('addGrape');
+const removeGrapesBtn = document.getElementById('removeGrape');
+const grapesDisplay = document.getElementById('displayGrape');
+const grapesPrice = document.getElementById('priceGrape');
+
+// Set initial count and price
 let count = 0;
+let itemPrice = 100.00;
 
-updateDisplay();
+let grapesCount = 0;
+let grapesItemPrice = 125.00;
 
-counterPlusElem.addEventListener("click",()=>{
-    count++;
-    updateDisplay();
-}) ;
-
-counterMinusElem.addEventListener("click",()=>{
-    count--;
-    updateDisplay();
+// Event listener for adding items
+addBtn.addEventListener('click', function() {
+  count++;
+  display.innerText = count;
+  price.innerText = (count * itemPrice).toFixed(2);
+  getTotal();
 });
 
-function updateDisplay(){
-    counterDisplayElem.innerHTML = count;
-    priceDisplayElem.innerHTML = (appleValue * count).toFixed(2);
-};
+// Event listener for removing items
+removeBtn.addEventListener('click', function() {
+  if (count > 0) {
+    count--;
+    display.innerText = count;
+    price.innerText = (count * itemPrice).toFixed(2);
+    getTotal();
+  }
+});
+
+// Event listener for adding Grapes Juice to cart
+addGrapesBtn.addEventListener('click', function() {
+  grapesCount++;
+  grapesDisplay.innerText = grapesCount;
+  grapesPrice.innerText = (grapesCount * grapesItemPrice).toFixed(2);
+  getTotal();
+});
+
+// Event listener for removing Grapes Juice from cart
+removeGrapesBtn.addEventListener('click', function() {
+  if (grapesCount > 0) {
+    grapesCount--;
+    grapesDisplay.innerText = grapesCount;
+    grapesPrice.innerText = (grapesCount * grapesItemPrice).toFixed(2);
+    getTotal();
+  }
+});
+
+function getTotal(){
+    let totalItems = parseInt(price.innerText) + parseInt(grapesPrice.innerText);
+    if(totalItems > 500.00){
+        totalItems = totalItems - (totalItems *0.10);
+    }
+    total.innerText = totalItems.toFixed(2);
+
+}
+
